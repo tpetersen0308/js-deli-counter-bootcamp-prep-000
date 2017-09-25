38c409beceb07748fc41de52d80a3b76d918683f
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-25 19:39:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,20 +4,26 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
+  if (katzDeliLine.length() === 0) {
     return "There is nobody waiting to be served!"
   }
   else {
      var name = katzDeliLine[0]
     katzDeliLine.shift()
-    return `Currently serving ${name}!`
+    return `Currently serving ${name}.`
   }
 }
 
 function currentLine(katzDeliLine) {
-  str = "The line is currently: "
-  for(var i = 0; i < katzDeliLine.length(); i++) {
-    str = str + (i + 1) + katzDeliLine[i]
+  var str = "The line is currently: "
+
+  if(katzDeliLine.length() === 0) {
+    return str + "empty."
+  }
+  else {
+    for(var i = 0; i < katzDeliLine.length(); i++) {
+      str = str + (i + 1) + katzDeliLine[i]
+    }
   }
   return str
 }
