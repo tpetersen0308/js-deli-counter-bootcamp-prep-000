1de68cc4e46ee8aacbc8ca7a3fca4b5fb9c25614
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-25 19:31:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing() {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    name = katzDeliLine[0]
+    katzDeliLine.shift()
+    return name
+  }
+}
